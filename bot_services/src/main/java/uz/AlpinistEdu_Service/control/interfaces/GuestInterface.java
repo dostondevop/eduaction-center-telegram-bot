1e821ee0038d7f362793f01f6d2476377e09ff72
MuxammadaminErkinboev <muxammadaminerkinboev6899@gmail.com>
2024-08-28 21:02:48 +0500

    .

--- bot_services/src/main/java/uz/AlpinistEdu_Service/control/interfaces/GuestInterface.java
+++ bot_services/src/main/java/uz/AlpinistEdu_Service/control/interfaces/GuestInterface.java
@@ -2,20 +2,32 @@ package uz.AlpinistEdu_Service.control.interfaces;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import uz.AlpinistEdu_Service.utils.BotUtil;
+
+import java.util.List;
+
+import static uz.AlpinistEdu_Service.utils.BotButtonsUtill.*;
 
 public class GuestInterface implements BaseInterface {
     @Override
     public ReplyKeyboard replyKeyboardStartMenu() {
-        return null;
+        return BotUtil.replyKeyboardMarkup(GUEST_MAIN_MENU_BUTTONS, 2);
     }
 
     @Override
     public ReplyKeyboard replyKeyboardSecondInnerMenu(String buttonName, Long chatId) {
+        List<String> buttons = SECOND_MENU_OPTIONS.get(buttonName);
+        if (buttons != null) {
+            return BotUtil.replyKeyboardMarkup(buttons, 2);
+        }
         return null;
     }
-
     @Override
     public SendMessage sendMessage(String buttonName, Long chatId) {
-        return null;
+        SendMessage sendMessage = new SendMessage();
+        String responseText = getInfo(buttonName);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(responseText);
+        return sendMessage;
     }
 }
--- bot_services/src/main/java/uz/AlpinistEdu_Service/service/UserService.java
+++ bot_services/src/main/java/uz/AlpinistEdu_Service/service/UserService.java
@@ -105,4 +105,6 @@ public class UserService implements BaseService<User>{
         users.set(index,user);
         write(users);
     }
+
+
 }
